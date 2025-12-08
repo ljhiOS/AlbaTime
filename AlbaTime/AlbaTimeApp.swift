4ad54c02898d1ad147800fbe6297d5bf99cfb65a
ljhiOS <ljh230c@naver.com>
2025-12-08 17:40:18 +0900

    Initial Commit

---
+++ AlbaTime/AlbaTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbaTimeApp.swift
+//  AlbaTime
+//
+//  Created by 이준희 on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbaTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
